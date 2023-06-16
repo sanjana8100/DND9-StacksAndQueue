32e89d29e02d028e4aa5f5aba34633807e428176
sanjana8100 <sanjanams812@gmail.com>
2023-06-16 23:02:55 +0530

    Stack And Queue

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DND9_StacksAndQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
